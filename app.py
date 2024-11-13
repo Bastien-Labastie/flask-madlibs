cad1d290ad32dc7a2e43ca2e20d21d035d225ea2
Bastien-Labastie <looney.labastie@gmail.com>
2024-11-13 12:11:34 -0500

    Venv

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 from stories import story
 
 app = Flask(__name__)
-app.config['SECRET KEY'] = 'Secret'
+app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
 
-@app.route('/')
-def question():
+
+@app.route("/")
+def ask_questions():
     prompts = story.prompts
 
-    return render_template("questions.html", promtps = prompts)
+    return render_template("questions.html", prompts=prompts)
 
-@app.route('/story')
-def theStory():
+@app.route("/story")
+def show_story():
     text = story.generate(request.args)
 
-    return render_template("story.html", text = text)
+    return render_template("story.html", text=text)
